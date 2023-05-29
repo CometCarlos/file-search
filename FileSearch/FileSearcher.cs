6dfce3897a523eabf8cdf7cc57464e017fbc5dbd
Carlos DaLomba <carlos@cometpeakgames.net>
2023-05-29 15:44:22 -0400

    Added original project with VS gitignore

---
+++ FileSearch/FileSearcher.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FileSearch {
+	public class FileSearcher {
+		private string rootPath;
+		private string searchTarget;
+		private bool ignoreCase;
+		private bool multiThreaded;
+		private Comparison<string> comparer;
+
+		public string RootPath {
+			get { return rootPath; }
+			set {
+				if (!Directory.Exists(value))
+					throw new DirectoryNotFoundException("The directory" + rootPath + " does not exist.");
+				rootPath = value;
+			}
+		}
+
+		public string SearchTarget {
+			get { return searchTarget; }
+			set { searchTarget = value; }
+		}
+
+		public bool IgnoreCase {
+			get { return ignoreCase; }
+			set { ignoreCase = value; }
+		}
+
+		public bool MultiThreaded {
+			get { return multiThreaded; }
+			set { multiThreaded = value; }
+		}
+
+		public FileSearcher(string rootPath, string searchTarget) {
+			RootPath = rootPath;
+			SearchTarget = searchTarget;
+		}
+
+		public void SetSortBy(Comparison<string> comparer) {
+			this.comparer = comparer;
+		}
+
+		public List<string> Search() {
+			List<string> pathResults = new List<string>(16);
+			string searchTarget = IgnoreCase ? SearchTarget.ToLower() : SearchTarget;
+			
+			if (MultiThreaded) {
+				try {
+					Task.Run(() => SearchMultiThreaded(RootPath, pathResults, searchTarget)).Wait();
+					Console.WriteLine("FINISHED ----------------------------------------------------");
+				} catch (AggregateException e) {
+					Console.WriteLine("Something went wrong in the process of setting up the parallel tasks!\n\n" + e.InnerException.Message + "\n\n" + e.InnerException.StackTrace);
+				}
+			} else {
+				Search(RootPath, pathResults, searchTarget);
+			}
+			if (comparer != null)
+				pathResults.Sort(comparer);
+
+			return pathResults;
+		}
+
+		private void Search(string currentPath, List<string> pathResults, string searchTarget) {
+			try {
+				string[] filePaths = Directory.GetFiles(currentPath);
+
+				for (int i = 0; i < filePaths.Length; i++) {
+					string currentFileName = Path.GetFileName(filePaths[i]);
+					if (IgnoreCase)
+						currentFileName = currentFileName.ToLower();
+
+					if (currentFileName.Contains(searchTarget))
+						pathResults.Add(filePaths[i]);
+				}
+
+				string[] directoryPaths = Directory.GetDirectories(currentPath);
+				for (int i = 0; i < directoryPaths.Length; i++)
+					Search(directoryPaths[i], pathResults, searchTarget);
+			} catch (UnauthorizedAccessException e) {
+				//Move on..
+			}
+		}
+
+		private async Task SearchMultiThreaded(string currentPath, List<string> pathResults, string searchTarget) {
+			Task[] tasks = null;
+			//await Task.Delay(1000);
+			try {
+				string[] filePaths = Directory.GetFiles(currentPath);
+
+				for (int i = 0; i < filePaths.Length; i++) {
+					string currentFileName = Path.GetFileName(filePaths[i]);
+					if (IgnoreCase)
+						currentFileName = currentFileName.ToLower();
+
+					if (currentFileName.Contains(searchTarget)) {
+						lock (pathResults) {
+							pathResults.Add(filePaths[i]);
+						}
+					}
+				}
+
+				string[] directoryPaths = Directory.GetDirectories(currentPath);
+				tasks = new Task[directoryPaths.Length];
+				for (int i = 0; i < directoryPaths.Length; i++) {
+					int threadSafeIndex = i;
+					tasks[i] = Task.Run(() => SearchMultiThreaded(directoryPaths[threadSafeIndex], pathResults, searchTarget));
+				}
+			} catch (UnauthorizedAccessException e) {
+				//Move on..
+			}
+			if (tasks != null) {
+				await Task.WhenAll(tasks);
+			}
+		}
+	}
+}
---
+++ FileSearch/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.IO;
+using System.Diagnostics;
+
+namespace FileSearch {
+	public class Program {
+		private const int FileCountPerPage = 10;
+
+		public static void Main(string[] args) {
+			//List<int> test = new List<int>();
+			//test.Add(3);
+			//test.Add(5);
+			//test.Add(7);
+
+			//test.Insert(3, 9);
+			//for (int i = 0; i < test.Count; i++)
+			//	Console.Write(test[i] + " ");
+
+			//Console.ReadKey(false);
+			//return;
+			Console.ForegroundColor = ConsoleColor.White;
+			Console.WriteLine("What directory would you like to search?");
+			string path = Console.ReadLine();
+			//Console.WriteLine("The directory that will be searched is:\n" + path);
+
+			Console.WriteLine("What would you like to search for in the file names?");
+			string searchTarget = Console.ReadLine();
+			Stopwatch s = new Stopwatch();
+			s.Start();
+
+			FileSearcher searcher = new FileSearcher(path, searchTarget);
+			searcher.IgnoreCase = true;
+			searcher.MultiThreaded = true;
+			searcher.SetSortBy((path1, path2) => {
+				return 0;
+			});
+			List<string> pathResults = searcher.Search();
+
+			Console.WriteLine(pathResults.Count + " results found!");
+			s.Stop();
+			Console.WriteLine("That took " + s.ElapsedMilliseconds + "ms!");
+
+			for (int i = 0; i < pathResults.Count; i += FileCountPerPage) {
+				for (int j = 0; j < FileCountPerPage; j++) {
+					int index = i + j;
+					Console.WriteLine("Result " + index + ":\n" + GetFileInfo(pathResults[i]) + "\n");
+				}
+			}
+			//for (int i = 0; i < pathResults.Count; i++) {
+			//	Console.WriteLine(pathResults[i]
+			//		+ "\n" + File.GetLastAccessTime(pathResults[i])
+			//		+ "\n");
+			//}
+			//Console.WriteLine("... ... ... and now " + pathResults.Count + " results!!");
+
+			Console.ReadKey(false);
+		}
+
+		private static string GetFileInfo(string path) {
+			string info = path;
+			info += "\nFile Name:" + string.Format("{0, 30}", Path.GetFileName(path));
+			info += "\nCreated: " + string.Format("{0, 30}", File.GetCreationTime(path));
+
+			return info;
+		}
+	}
+}
